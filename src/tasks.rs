e6d43766ed24b40eec09b55f9a7458089f86be56
Morten Bergseng <bergseng.m@gmail.com>
2022-06-06 16:59:08 +0200

    added remove task

--- src/tasks.rs
+++ src/tasks.rs
@@ -2,7 +2,7 @@ use chrono::{serde::ts_seconds, DateTime, Local, Utc};
 use serde::Deserialize;
 use serde::Serialize;
 use std::fs::{File, OpenOptions};
-use std::io::{Result, Seek, SeekFrom};
+use std::io::{Error, ErrorKind, Result, Seek, SeekFrom};
 use std::path::PathBuf;
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -41,4 +41,31 @@ pub fn add_task(jounal_path: PathBuf, task: Task) -> Result<()> {
     serde_json::to_writer(file, &tasks)?;
 
     Ok(())
+}
+
+pub fn complete_task(jounal_path: PathBuf, task_position: usize) -> Result<()> {
+    // open file
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(jounal_path)?;
+
+    //consume firel's contents as vector of tasks
+    let tasks = match serde_json::from_reader(file) {
+        Ok(tasks) => tasks,
+        Err(e) if e.is_eof() => Vec::new(),
+        Err(e) => Err(e)?,
+    };
+
+    if task_position == 0 || task_position > tasks.len() {
+        return Err(Error::new(ErrorKind::InvalidInput, "Invalid task ID"));
+    }
+    tasks.remove(task_position - 1);
+
+    //Rewind and truncate the file
+    file.seek(SeekFrom::Start(0))?;
+    file.set_len(0)?;
+
+    //Write the modified task list back into the file
+    serde_json::to_writer(file, &tasks)?;
 }
