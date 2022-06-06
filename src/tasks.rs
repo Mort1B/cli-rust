be8d31bf9a0310d30ab1f2deee55a1611877d6ff
Morten Bergseng <bergseng.m@gmail.com>
2022-06-06 17:21:36 +0200

    done

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,6 +1,7 @@
 use chrono::{serde::ts_seconds, DateTime, Local, Utc};
 use serde::Deserialize;
 use serde::Serialize;
+use std::fmt;
 use std::fs::{File, OpenOptions};
 use std::io::{Error, ErrorKind, Result, Seek, SeekFrom};
 use std::path::PathBuf;
@@ -14,13 +15,13 @@ pub struct Task {
 }
 
 impl Task {
-    pub fn new(text: String) -> Taks {
+    pub fn new(text: String) -> Task {
         let created_at: DateTime<Utc> = Utc::now();
         Task { text, created_at }
     }
 }
 
-pub fn add_task(jounal_path: PathBuf, task: Task) -> Result<()> {
+pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
     //Open the file
     let mut file = OpenOptions::new()
         .read(true)
@@ -34,12 +35,12 @@ pub fn add_task(jounal_path: PathBuf, task: Task) -> Result<()> {
     Ok(())
 }
 
-pub fn complete_task(jounal_path: PathBuf, task_position: usize) -> Result<()> {
+pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
     // open file
     let file = OpenOptions::new()
         .read(true)
         .write(true)
-        .open(jounal_path)?;
+        .open(journal_path)?;
 
     //consume firel's contents as vector of tasks
     let mut tasks = collect_tasks(&file)?;
@@ -68,7 +69,7 @@ fn collect_tasks(mut file: &File) -> Result<Vec<Task>> {
     Ok(tasks)
 }
 
-pub fn list_tasks(jounal_path: PathBuf) -> Result<()> {
+pub fn list_tasks(journal_path: PathBuf) -> Result<()> {
     //Open file
     let file = OpenOptions::new().read(true).open(journal_path)?;
     // parse file => collect tasks
@@ -85,4 +86,11 @@ pub fn list_tasks(jounal_path: PathBuf) -> Result<()> {
         }
     }
     Ok(())
+}
+
+impl fmt::Display for Task {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let created_at = self.created_at.with_timezone(&Local).format("%F %H:%M");
+        write!(f, "{:<50} [{}]", self.text, created_at)
+    }
 }
